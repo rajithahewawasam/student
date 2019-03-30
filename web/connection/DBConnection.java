db44126dc5a1c8cf028d5fbc6caf4f11a8a3a794
RAJI <RAJI@RAJITHA.local>
2019-03-30 13:31:27 +0530

    Created database connection through the mysql

---
+++ web/connection/DBConnection.java
@@ -0,0 +1,26 @@
+package connection;
+
+import java.sql.Connection;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RAJI
+ */
+public class DBConnection {
+    
+    
+    public static Connection connector()
+    {
+        
+        return null;
+    
+    
+    }
+    
+}
